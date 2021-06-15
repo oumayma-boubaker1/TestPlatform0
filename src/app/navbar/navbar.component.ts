5d104217dd1a2ae107c600f7757315fbaa7aaa64
Oumayma Boubaker <oumayma.boubaker@gmail.com>
2021-06-15 01:57:49 +0100

    update navbar

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnDestroy, OnInit } from '@angular/core';
+// import { AuthService } from '../service/auth.service';
+// import { ActivatedRoute } from '@angular/router';
+// import { Subscription } from 'rxjs';
+// import { ProfileService } from '../service/profile.service';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
@@ -7,9 +10,61 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  // userIsAuthenticated = false;
+  // private authListenerSubs: Subscription;
+  // profile: any;
+  // username: string
+  // profileisSet = false
+  // constructor(private authService: AuthService, private profileService: ProfileService,
+  //             private route: ActivatedRoute) { }
 
-  ngOnInit() {
-  }
+   ngOnInit(): void {
 
-}
+
+  //   this.profileisSet = this.profileService.getIsProfileSet()
+  //   this.userIsAuthenticated = this.authService.getIsAuth();
+  //   if (this.userIsAuthenticated) {
+  //     this.getProfile()
+  //   }
+
+  //   this.authListenerSubs = this.authService
+  //     .getAuthStatusListener()
+  //     .subscribe(isAuthenticated => {
+  //       this.userIsAuthenticated = isAuthenticated;
+  //       if (this.userIsAuthenticated) {
+  //         this.getProfile()
+  //       }
+  //     });
+  // }
+
+
+  // onLogout() {
+  //   this.authService.logout();
+  // }
+
+  // getProfile() {
+  //   this.profileService.getProfileByCreatorId().subscribe(prof => {
+  //     this.profileisSet = true
+  //     this.username = prof.profile.username
+  //     this.profile = {
+  //       id: prof.profile._id,
+  //       username: prof.profile.username,
+  //       bio: prof.profile.bio,
+  //       imagePath: prof.profile.imagePath,
+  //       creator: prof.profile.creator
+  //     };
+  //   },
+  //     err => {
+  //       this.profileisSet = false
+  //       this.username = null
+  //     })
+
+  // }
+
+
+  // // tslint:disable-next-line:use-life-cycle-interface
+  // ngOnDestroy() {
+  //   this.authListenerSubs.unsubscribe();
+  // }
+
+}}
