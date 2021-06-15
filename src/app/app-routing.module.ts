5e92dc7977c18f2540ef4246069492ba061d1853
Oumayma Boubaker <oumayma.boubaker@gmail.com>
2021-06-15 01:58:56 +0100

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,17 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { LoginComponent } from './login/login.component';
-import { SignupComponent } from './signup/signup.component';
+import { LoginComponent } from './auth/login/login.component';
+// import { LoginnComponent } from './auth/loginn/loginn.component';
+import { SignupComponent } from './auth/signup/signup.component';
 import { NewQuizComponent } from './new-quiz/new-quiz.component';
 
 const routes: Routes = [
-  { path: '', component: LoginComponent},
-  { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignupComponent },
+  // { path: '', component: LoginComponent},
+ // { path: 'loginn', component: LoginnComponent },
+  { path: 'login', component: LoginComponent },
+ // { path: 'signup', component: SignupComponent },
   { path: 'home', component: HomeComponent },
   { path: 'dashboard', component: DashboardComponent },
   { path: 'newquiz', component: NewQuizComponent  },
